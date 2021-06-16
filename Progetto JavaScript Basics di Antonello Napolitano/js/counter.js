f5b1b4c5ec324b0eda59b83d81aa4d064735bce0
totomagia <81924899+totomagia@users.noreply.github.com>
2021-06-16 15:24:59 +0200

    Add files via upload
    My first web app made with Javascript. 'Counter for beginners' is a web app that acts as a counter. It has two buttons (+, -) that increase or decrease the initial value ("0"). Let's go and try it!

---
+++ Progetto JavaScript Basics di Antonello Napolitano/js/counter.js
@@ -0,0 +1,13 @@
+var x=0;
+function increase() {
+ document.getElementsByClassName('count')[0].innerHTML = x++;
+}
+
+
+var x=0;
+function decrease() {
+ document.getElementsByClassName('count')[0].innerHTML = --x;
+}
+
+document.write(0);
+
