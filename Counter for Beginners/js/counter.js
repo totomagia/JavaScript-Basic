bd294d973f3b7cf8c609de05a5f533431205750b
totomagia <81924899+totomagia@users.noreply.github.com>
2021-06-28 11:44:34 +0200

    Add files via upload

--- Counter for Beginners/js/counter.js
+++ Counter for Beginners/js/counter.js
@@ -1,13 +1,22 @@
 var x=0;
+var btn = document.createElement('button');
+var test = document.getElementById('button');
+test.addEventListener('click', increase);
+test.addEventListener('click', decrease);
+
+
 function increase() {
  document.getElementsByClassName('count')[0].innerHTML = x++;
 }
 
 
-var x=0;
 function decrease() {
  document.getElementsByClassName('count')[0].innerHTML = --x;
 }
 
 document.write(0);
+
+
+
+
 
