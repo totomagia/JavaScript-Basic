f45d2307ec28caa28975f101a215cd2bc221eb0b
totomagia <81924899+totomagia@users.noreply.github.com>
2021-07-08 17:00:28 +0200

    Update counter.js

--- Counter for Beginners/js/counter.js
+++ Counter for Beginners/js/counter.js
@@ -1,18 +1,29 @@
+var btnMinus = document.createElement('button');
+var textMinus = document.createTextNode('-');
+btnMinus.appendChild(textMinus);
+var positionMinus = document.getElementById('minus');
+positionMinus.appendChild(btnMinus);
+btnMinus.className='buttonProperties';
+
+
+var btnPlus = document.createElement('button');
+var textPlus = document.createTextNode('+');
+btnPlus.appendChild(textPlus);
+var positionPlus = document.getElementById('plus');
+positionPlus.appendChild(btnPlus);
+btnPlus.className='buttonProperties'
+
+
 var x=0;
-var btn = document.createElement('button');
-var test = document.getElementById('button');
-test.addEventListener('click', increase);
-test.addEventListener('click', decrease);
-
-
 function increase() {
  document.getElementsByClassName('count')[0].innerHTML = x++;
 }
-
+btnPlus.addEventListener('click', increase);
 
 function decrease() {
  document.getElementsByClassName('count')[0].innerHTML = --x;
 }
+btnMinus.addEventListener('click', decrease);
 
 document.write(0);
 
